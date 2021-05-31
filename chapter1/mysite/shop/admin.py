2defc06f2eb4bfab147ab197089d1543028ca54f
misszahrahshemi.github.io <you@example.com>
2021-05-31 09:39:57 -0700

    shop added

--- chapter1/mysite/shop/admin.py
+++ chapter1/mysite/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Shop
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'user', 'publish')
+    list_filter = ('created', 'publish', 'user')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('user',)
+    date_hierarchy = 'publish'
+    ordering = ('publish',)
