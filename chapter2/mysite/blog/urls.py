8882f77962d0f6d178bdb20b4986e0be1f951cec
misszahrahshemi.github.io <you@example.com>
2021-06-05 23:12:00 -0700

    forms.v1

---
+++ chapter2/mysite/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import post_list, post_detail, post_share
+
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        post_detail,
+        name='post_detail'),
+    path('<int:post_id>/share/',
+        post_share, name='post_share'),
+]
