9ba34ce3019cda47f5bd2547ab2bca58ef22c817
misszahrahshemi.github.io <you@example.com>
2021-06-12 23:18:02 -0700

    Tags-v2

--- chapter2/mysite/blog/views.py
+++ chapter2/mysite/blog/views.py
@@ -3,10 +3,15 @@ from .models import Post, Comment
 from .forms import EmailPostForm ,CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-
-def post_list(request):
+def post_list(request, tag = None):
     object_list = Post.objects.filter(status='published')
+    tag = None
+    
+    if "tag_slug":
+        tag = get_object_or_404(Tag, slug="tag_slug")
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 2)  # 3 posts in each page
     try:
         page = request.GET.get('page')
@@ -23,7 +28,8 @@ def post_list(request):
 
     return render(request,  'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
