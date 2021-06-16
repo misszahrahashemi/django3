04561717c740e370c8f6a27ccdbb9bd0200a2ffa
misszahrahshemi.github.io <you@example.com>
2021-06-16 10:36:34 -0700

    forms-v1

--- chapter2/mysite/blog/urls.py
+++ chapter2/mysite/blog/urls.py
@@ -5,11 +5,11 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-        post_detail,
-        name='post_detail'),
-    path('<int:post_id>/share/',
-        post_share, name='post_share'),
-    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+     path('', post_list, name='post_list'),
+     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         post_detail,
+         name='post_detail'),
+     path('<int:post_id>/share/',
+         post_share, name='post_share'),
+     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
 ]
