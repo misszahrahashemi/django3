fcda9a859473b9e0d922327ed5235e57b22280d7
misszahrahshemi.github.io <you@example.com>
2021-06-26 23:39:22 -0700

    account -v1

--- chapter2/mysite/bookmarks/account/models.py
+++ chapter2/mysite/bookmarks/account/models.py
@@ -1,15 +1,11 @@
-# Create your models here.
 from django.db import models
 from django.conf import settings
-
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+ user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
-                                
-date_of_birth = models.DateField(blank=True, null=True)
-photo = models.ImageField(upload_to='users/%Y/%m/%d/',
-                          blank=True)
-
-
+ date_of_birth = models.DateField(blank=True, null=True)
+ photo = models.ImageField(upload_to='users/%Y/%m/%d/',
+                            blank=True)
 def __str__(self):
-    return f'Profile for user {self.user.username}'
+ return f'Profile for user {self.user.username}'
+# Create your models here.
